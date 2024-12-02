f674445c1208dab1f7aee6e985ce72a57edcefb7
FaelPavani <rafael.ssilva@sptech.school>
2024-12-02 20:19:31 -0300

    subindo site beta

---
+++ Rzone/src/database/script-tabelas.sql
@@ -0,0 +1,5 @@
+create database RapZone;
+
+use RapZone;
+
+create table tb_usuario (id_usuario int primary key auto_increment, nome_usuario varchar(45), email_usuario varchar(45), senha_usuario varchar(100));
